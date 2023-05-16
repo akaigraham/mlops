d230d88b9d5688a478c4e076442a6a27a07d1d9d
Addington Graham <akaigraham@gmail.com>
2023-05-16 14:17:24 -0400

    read dataframe function

--- orchestration/prefect_flow.py
+++ orchestration/prefect_flow.py
@@ -13,4 +13,21 @@ import mlflow
 
 from prefect import flow, task
 
+@task
+def read_dataframe(filename):
+    df = pd.read_parquet(filename)
+
+    df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
+    df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+
+    df['duration'] = df.lpep_dropoff_datetime - df.lpep_pickup_datetime
+    df.duration = df.duration.apply(lambda td: td.total_seconds() / 60)
+
+    df = df[(df.duration >= 1) & (df.duration <= 60)]
+
+    categorical = ['PULocationID', 'DOLocationID']
+    df[categorical] = df[categorical].astype(str)
+    
+    return df
+
 
