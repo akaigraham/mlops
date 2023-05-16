e7fdc890be04098221847638111a348de155c1a4
Addington Graham <akaigraham@gmail.com>
2023-05-16 14:05:45 -0400

    initial commit

---
+++ orchestration/prefect_flow.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import pickle 
+
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.metrics import mean_squared_error 
+
+import xgboost as xgb 
+
+from hyperopt import fmin, tpe, hp, STATUS_OK, Trials 
+from hyperopt.pyll import scope 
+
+import mlflow 
+
+from prefect import flow, task
