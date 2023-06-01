61044d35f02347d64ce3cce8ae032d197e7633d7
Addington Graham <akaigraham@gmail.com>
2023-06-01 17:24:44 -0400

    train best model

--- nyc-taxi/scripts/orchestration_pre_prefect.py
+++ nyc-taxi/scripts/orchestration_pre_prefect.py
@@ -63,3 +63,52 @@ def add_features(
     y_val = df_val[target].values
     
     return X_train, X_val, y_train, y_val, dv
+
+def train_best_model(
+    X_train: scipy.sparse._csr.csr_matrix,
+    X_val: scipy.sparse._csr.csr_matrix,
+    y_train: np.ndarray,
+    y_val: np.ndarray,
+    dv: sklearn.feature_extraction.DictVectorizer
+) -> None:
+    """
+    Train a model with the best hyperparams and write
+    everything out.
+    """
+    
+    # start mlflow run
+    with mlflow.start_run():
+        train = xgb.DMatrix(X_train, label=y_train)
+        valid = xgb.DMatrix(X_val, label=y_val)
+        
+        best_params = {
+            "learning_rate": 0.09585355369315604,
+            "max_depth": 30,
+            "min_child_weight": 1.060597050922164,
+            "objective": "reg:linear",
+            "reg_alpha": 0.018060244040060163,
+            "reg_lambda": 0.011658731377413597,
+            "seed": 42, 
+        }
+        
+        mlflow.log_params(best_params)
+
+        booster = xgb.train(
+            params=best_params,
+            dtrain=train,
+            num_boost_round=100,
+            evals=[(valid, "validation")],
+            early_stopping_rounds=20
+        )
+        
+        y_pred = booster.predict(valid)
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+        mlflow.log_metrics('rmse', rmse)
+        
+        Path("../models").mkdir(exist_ok=True)
+        with open("../models/preprocessor.b", "wb") as f_out:
+            pickle.dump(dv, f_out)
+        mlflow.log_artifact("models/preprocessor.b", artifact_path="preprocessor")
+        mlflow.xgboost.log_model(booster, artifact_path="models_mlflow") 
+    
+    return None
