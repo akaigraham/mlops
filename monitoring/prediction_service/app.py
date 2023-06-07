34a1c7c5c36f7911b1b88625efaf3f6f22829583
Addington Graham <akaigraham@gmail.com>
2023-06-07 15:00:21 -0400

    load model

--- monitoring/prediction_service/app.py
+++ monitoring/prediction_service/app.py
@@ -8,3 +8,10 @@ from flask import jsonify
 
 from pymongo import MongoClient 
 
+MODEL_FILE = os.getenv('MODEL_FILE', 'lin_reg.bin')
+EVIDENTLY_SERVICE_ADDRESS = os.getenv('EVIDENTLY_SERVICE', 'http://127.0.0.1:5000')
+MONGODB_ADDRESS = os.getenv("MONGODB_ADDRESS", "mongodb://127.0.0.1:27017")
+
+with open(MODEL_FILE, 'rb') as f_in:
+    dv, model = pickle.load(f_in)
+    
